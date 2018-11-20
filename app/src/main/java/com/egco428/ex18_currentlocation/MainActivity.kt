010b721a224568f075289a582e997b6a4cb44694
6272 <a6272@6272s-Mac-mini.local>
2018-11-20 11:31:16 +0700

    Initial commit

---
+++ app/src/main/java/com/egco428/ex18_currentlocation/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.egco428.ex18_currentlocation
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    private var locationManager: LocationManager?  = null
+    private var locationListener: LocationListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(location: Location?) {
+                textView.append("\n"+location!!.longitude+"\n"+location.latitude)
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+                val i = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(i)
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+            }
+        }
+        requestLocation()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            10 -> requestLocation()
+            else -> {  }
+        }
+    }
+    private fun requestLocation(){
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.INTERNET),10)
+
+            }
+            return
+        }
+        gpsBtn.setOnClickListener {
+            locationManager!!.requestLocationUpdates("gps",5000,0f,locationListener)
+        }
+    }
+}
